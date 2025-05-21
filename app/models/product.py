4271192761315bd3461111fd6a6d8a20505303b8
mikolajpasieka <pasiekamikolaj01@gmail.com>
2025-05-22 00:19:31 +0200

    2025-05-22 MP

--- app/models/product.py
+++ app/models/product.py
@@ -1,76 +1,81 @@
-import json
-import pandas as pd
 import os
+import json
 import requests
-form bs4 import BeautifulSoup
-form config impor headers
-form app 
+import pandas as pd
+from bs4 import BeautifulSoup
+from config import headers
+from app.utils import extract
+from app.models.review  import Review
 
 class Product:
-    def __init__(self, product_id, reviews = [], product_name = "", stats = {}):
-        self.product_id = prduct_id
+    def __init__(self, product_id, reviews=[], product_name="", stats = {}):
+        self.product_id = product_id
         self.reviews = reviews
         self.product_name = product_name
-        self.stats = stats 
+        self.stats = stats
 
     def __str__(self):
-        return f""" 
-                product_id : {self.product_id} 
-                product_name : {self.product_name}
-                reviews : {("\n\n").join(str(review)) for review in  self.reviews}
-                stats: {json.dump(self.stats indent = 4, ensure_ascii = False)}
-            """
-
+        nl="\n\n"
+        return f"""product_id: {self.product_id}
+            product_name: {self.product_name}
+            stats: {json.dumps(self.stats, indent=4, ensure_ascii=False)}
+            reviews: {nl.join([str(review) for review in self.reviews])}"""
+    
     def reviews_to_dict(self):
-        return [reviews.to.dict() for review in sel.reviews]
+        return [review.to_dict() for review in self.reviews]
 
     def info_to_dict(self):
         return {
-            "product_id" : self.product_id
-            "product_name" : self.product_name 
+            "product_id": self.product_id,
+            "product_name": self.product_name,
             "stats": self.stats
         }
 
-    def  extract_name(self):
-        pass
+    def extract_name(self):
+        next_page = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        response = requests.get(next_page, headers=headers)
+        if response.status_code == 200:
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            self.product_name = extract(page_dom, "h1")
+        else:
+            self.product_name = ""
+        return self
 
-    def  extract_reviews(self):
-            next_page = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
-            while next_page:
-                response = requests.get(next_page, headers=headers)
-                print(next_page)
-                if response.status_code == 200:
-                    page_dom = BeautifulSoup(response.text, "html.parser")
-                    reviews = page_dom.select("div.js_product-review:not(.user-post--highlight)")
-                    print(len(reviews))
-                    for review in reviews:
+    def extract_reviews(self):
+        next_page = f"https://www.ceneo.pl/{self.product_id}#tab=reviews"
+        while next_page:
+            response = requests.get(next_page, headers=headers)
+            print(next_page)
+            if response.status_code == 200:
+                page_dom = BeautifulSoup(response.text, "html.parser")
+                reviews = page_dom.select("div.js_product-review:not(.user-post--highlight)")
+                print(len(reviews))
+                for review in reviews:
+                    single_review = Review()
+                    self.reviews.append(single_review.extract_features(review).transform())
+                try:
+                    next_page = "https://www.ceneo.pl"+extract(page_dom, "a.pagination__next", "href")
+                except TypeError:
+                    next_page = None     
+        return self  
 
-                    try:
-                        next_page = "https://www.ceneo.pl"+extract(page_dom, "a.pagination__next", "href")
-                    except TypeError:
-                        next_page = None       
-
-    def calculate_stats(selff):
-        reviews = pd.DataFrame.form.to_dict(self.reviews_to_dict())
-        self.stats [revies_count] = reviews.shape[0]
-        self.stats [pros_count] = reviews.pros.astype(bool).sum()
-        self.stats [cons_count] =  reviews.cons.astype(bool).sum()
-        self.stats [pros_cons_count] = reviews.apply(lambda r: bool(r.pros) and bool(r.cons), axis=1).sum()
-        self.stats [average_count] = round(reviews.stars.mean(),2)
-       
+    def calculate_stats(self):
+        reviews = pd.DataFrame.from_dict(self.reviews_to_dict())
+        self.stats["reviews_count"] = int(reviews.shape[0])
+        self.stats["pros_count"] = int(reviews.pros.astype(bool).sum())
+        self.stats["cons_count"] = int(reviews.cons.astype(bool).sum())
+        self.stats["pros_cons_count"] = int(reviews.apply(lambda r: bool(r.pros) and bool(r.cons), axis=1).sum())
+        self.stats["average_stars"] = round(reviews.stars.mean(),2)
+        return self
 
     def export_reviews(self):
-        if not os.path.exists("./data/opinion"):
-            os.mkdir("./data/opinion")
-        with open(f"./data/opinion/{self.product_id}.json", "w", encoding="UTF-8") as jf: 
+        if not os.path.exists("./app/data/opinions"):
+            os.mkdir("./app/data/opinions")
+        with open(f"./app/data/opinions/{self.product_id}.json", "w", encoding="UTF-8") as jf:
             json.dump(self.reviews_to_dict(), jf, indent=4, ensure_ascii=False)
 
     def export_info(self):
-        if not os.path.exists("./data/info"):
-            os.mkdir("./data/info")
-        with open(f"./data/info/{self.product_id}.json", "w", encoding="UTF-8") as jf: 
+        if not os.path.exists("./app/data/products"):
+            os.mkdir("./app/data/products")
+        with open(f"./app/data/products/{self.product_id}.json", "w", encoding="UTF-8") as jf:
             json.dump(self.info_to_dict(), jf, indent=4, ensure_ascii=False)
-
-    def extract_features (self):
-        for keyy, value on sel.review_schema.keys()
-
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,36 @@
-from flask import render_template
-
+from flask import render_template, redirect, url_for, request
 from app import app
+from app.models.product import Product
 
 @app.route("/")
-    def index():
-        return render_template("index.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/extract")
-    def extract():
-        return render_template("extract.html")
+def display_form():
+    return render_template("extract.html")
+
+@app.route("/extract", methods=['POST'])
+def extract():
+    product_id=request.form.get("product_id")
+    product = Product(product_id)
+    product.extract_reviews().extract_name().calculate_stats()
+    product.export_reviews()
+    product.export_info()
+    return redirect(url_for('product', product_id=product_id))
 
 @app.route("/products")
-    def products():
-        return render_template("products.html")
+def products():
+    return render_template("products.html")
 
 @app.route("/product/<product_id>")
-    def product(product_id):
-        return render_template("product.html", product_id=product_id)
+def product(product_id):
+    return render_template("product.html", product_id=product_id)
 
 @app.route("/charts/<product_id>")
-    def charts(product_id):
-        return render_template("charts.html", product_id=product_id)
+def charts(product_id):
+    return render_template("charts.html", product_id=product_id)
 
 @app.route("/author")
-    def author():
-        return render_template("author.html")
-
-
+def author():
+    return render_template("author.html")
